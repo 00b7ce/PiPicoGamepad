c648aa9b8e9254566bda92e1c30f0323269ed750
mekabu <sleep.seep.sheep@gmail.com>
2021-12-30 22:59:45 +0900

    レポートIDの追加

--- src/main.c
+++ src/main.c
@@ -1,59 +1,8 @@
-/* 
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "PiPicoGamepad.h"
-
-/* This example demonstrate HID Generic raw Input & Output.
- * It will receive data from Host (In endpoint) and echo back (Out endpoint).
- * HID Report descriptor use vendor for usage page (using template TUD_HID_REPORT_DESC_GENERIC_INOUT)
- *
- * There are 2 ways to test the sketch
- * 1. Using nodejs
- * - Install nodejs and npm to your PC
- *
- * - Install excellent node-hid (https://github.com/node-hid/node-hid) by
- *   $ npm install node-hid
- *
- * - Run provided hid test script
- *   $ node hid_test.js
- *
- * 2. Using python
- * - Install `hid` package (https://pypi.org/project/hid/) by
- *   $ pip install hid
- *
- * - hid package replies on hidapi (https://github.com/libusb/hidapi) for backend,
- *   which already available in Linux. However on windows, you may need to download its dlls from their release page and
- *   copy it over to folder where python is installed.
- *
- * - Run provided hid test script to send and receive data to this device.
- *   $ python3 hid_test.py
- */
 
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
@@ -95,7 +44,7 @@ int main(void)
 //--------------------------------------------------------------------+
 // HID report
 //--------------------------------------------------------------------+
-bool mkb_hid_n_gamepad_report(uint8_t instance, uint8_t report_id, int8_t x, int8_t y, int8_t z, int8_t rz, int8_t rx, int8_t ry, uint8_t hat, uint32_t buttons)
+bool mkb_hid_n_gamepad_report(uint8_t instance, uint8_t report_id, int8_t x, int8_t y, int8_t rx, int8_t ry, uint8_t hat, uint32_t buttons)
 {
   gamepad_report_t report =
   {
@@ -125,7 +74,7 @@ void hid_task(void)
   if (tud_hid_n_ready(ITF_GAMEPAD))
   {
     // use to avoid send multiple consecutive zero report for keyboard
-    mkb_hid_n_gamepad_report(ITF_GAMEPAD, 4, 0, 0, 0, 0, 0, 0, GAMEPAD_HAT_UP, GAMEPAD_BUTTON_A);
+    mkb_hid_n_gamepad_report(ITF_GAMEPAD, REPORT_ID_GAMEPAD, 0, 0, 0, 0, GAMEPAD_HAT_UP, GAMEPAD_BUTTON_A);
   }
 }
 
